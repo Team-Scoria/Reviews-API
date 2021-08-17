970f62ca7ebea7292ce96c99031b8733c144d247
shannamurry <shanna.murry84@gmail.com>
2021-08-17 18:43:08 -0400

    queries finalized

--- queries.js
+++ queries.js
@@ -11,88 +11,105 @@ module.exports = {
     if (sort === 'newest') {
       queryString = `SELECT *,
       (SELECT COALESCE (json_agg((photos)), '[]'::json) FROM photos where review_id = reviews.id) AS photos
-      FROM reviews WHERE product_id = ${product_id} ORDER BY date DESC OFFSET ${(count * page) - count} LIMIT ${count}`;
+      FROM reviews WHERE product_id = ${product_id} AND reported = false ORDER BY date DESC OFFSET ${(count * page) - count} LIMIT ${count}`;
 
     } else if (sort === 'helpful') {
       queryString = `SELECT *,
       (SELECT COALESCE (json_agg(photos), '[]'::json) FROM photos where review_id = reviews.id) AS photos
-      FROM reviews WHERE product_id = ${product_id} ORDER BY helpfulness DESC OFFSET ${(count * page) - count} LIMIT ${count}`;
+      FROM reviews WHERE product_id = ${product_id} AND reported = false ORDER BY helpfulness DESC OFFSET ${(count * page) - count} LIMIT ${count}`;
 
     } else {
       queryString = `SELECT *,
       (SELECT COALESCE (json_agg(photos), '[]'::json) FROM photos where review_id = reviews.id) AS photos
-      FROM reviews WHERE product_id = ${product_id} ORDER BY helpfulness DESC, date DESC OFFSET ${(count * page) - count} LIMIT ${count}`;
+      FROM reviews WHERE product_id = ${product_id} AND reported = false ORDER BY helpfulness DESC, date DESC OFFSET ${(count * page) - count} LIMIT ${count}`;
     }
 
     db.query(queryString)
       .then(result => {
-        res.send(result.rows);
+        res.send({
+          'product': product_id,
+          'page': page,
+          'count': count,
+          'results': result.rows
+        });
       })
       .catch(err => {
         console.log(err);
       });
   },
 
-  getMetaData: function (req, res) {
+  getMetaData: async (req, res) => {
     let product_id = req.query.product_id || 1;
-    var ratingsResults;
-    var product_idResult;
-    var characteristicsResults;
-    charObj = {};
-    var recommendObj = {};
-    var responseObj = {};
 
-    var product_idQuery = `SELECT product_id FROM reviews WHERE product_id = ${product_id} limit 1`;
+    let ratingQuery = `SELECT COUNT(rating) AS "5",
+      (SELECT COUNT(rating) AS "4"  FROM reviews WHERE product_id = ${product_id} AND rating = 4),
+      (SELECT COUNT(rating) AS "3"  FROM reviews WHERE product_id = ${product_id} AND rating = 3),
+      (SELECT COUNT(rating) AS "2"  FROM reviews WHERE product_id = ${product_id} AND rating = 2),
+      (SELECT COUNT(rating) AS "1"  FROM reviews WHERE product_id = ${product_id} AND rating = 1)
+      FROM reviews WHERE product_id = ${product_id} AND rating = 5`;
 
-    var ratingQuery = `SELECT COUNT(rating) AS "5",
-    (SELECT COUNT(rating) AS "4"  FROM reviews WHERE product_id = ${product_id} AND rating = 4),
-    (SELECT COUNT(rating) AS "3"  FROM reviews WHERE product_id = ${product_id} AND rating = 3),
-    (SELECT COUNT(rating) AS "2"  FROM reviews WHERE product_id = ${product_id} AND rating = 2),
-    (SELECT COUNT(rating) AS "1"  FROM reviews WHERE product_id = ${product_id} AND rating = 1)
-    FROM reviews WHERE product_id = ${product_id} AND rating = 5`;
-
-    var characteristicsQuery = `SELECT name, id,
+    let characteristicsQuery = `SELECT name, id,
     (SELECT AVG(value) FROM characteristic_reviews WHERE characteristic_id = characteristics.id) AS average
     FROM characteristics WHERE product_id = ${product_id}`;
 
-    var recommendQueryTrue = `SELECT COUNT(recommend) FROM reviews WHERE product_id = ${product_id} AND recommend = true`;
-    var recommendQueryFalse = `SELECT COUNT(recommend) FROM reviews WHERE product_id = ${product_id} AND recommend = false`;
+    let recommendQueryTrue = `SELECT COUNT(recommend) AS true, (SELECT COUNT(recommend) as false FROM reviews WHERE product_id = ${product_id} AND recommend = false) FROM reviews WHERE product_id = ${product_id} AND recommend = true`;
 
+    let ratingsData = await db.query(ratingQuery);
+    let characteristicsData = await db.query(characteristicsQuery);
+    let recommendData = await db.query(recommendQueryTrue);
 
-    db.query(product_idQuery)
+    characteristicsObj = {};
+
+    characteristicsData.rows.forEach(function(item) {
+      characteristicsObj[item.name] = {id: item.id, value: item.average};
+    })
+
+    res.send({
+      product_id: product_id,
+      ratings: ratingsData.rows[0],
+      characterisctics: characteristicsObj,
+      recommended: recommendData.rows[0]
+    })
+  },
+
+  updateHelpful: function (req, res) {
+    let review_id = req.params.review_id;
+
+    let queryString = `UPDATE reviews SET helpfulness = helpfulness + 1 WHERE id = ${review_id}`;
+
+    db.query(queryString)
       .then(result => {
-        if (result.rows[0]) {
-          responseObj.product_id = result.rows[0].product_id;
-          db.query(ratingQuery)
-            .then(newResult => {
-              responseObj.ratings = newResult.rows[0];
-              db.query(characteristicsQuery)
-                .then(charResult => {
-                  charResult.rows.forEach(function (characteristic) {
-                    charObj[characteristic.name] = { "id": characteristic.id, "value": characteristic.average } || {};
-                  })
-                  responseObj.characteristics = charObj;
-                  db.query(recommendQueryTrue)
-                    .then(trueResult => {
-                      recommendObj['true'] = trueResult.rows[0].count;
-                      db.query(recommendQueryFalse)
-                        .then(falseResult => {
-                          recommendObj['false'] = falseResult.rows[0].count;
-                          responseObj.recommended = recommendObj;
-                        })
-                        .then(result => {
-                          res.send(responseObj);
-                        })
-                    })
-                })
-            });
-        } else {
-          // if there are no reviews and no metadata for this object
-          res.send({});
-        }
+        res.send();
       })
       .catch(err => {
-        console.log(err);
+        console.log(err)
+      });
+  },
+
+  updateReport: function (req, res) {
+    let review_id = req.params.review_id;
+
+    let queryString = `UPDATE reviews SET reported = true WHERE id = ${review_id}`;
+
+    db.query(queryString)
+      .then(result => {
+        res.send();
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  },
+
+  createReview: function (req, res) {
+    let reviewsPostQuery = 'INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reviewer_name, reviewer_email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+    let postParams = [req.body.product_id, req.body.rating, 'NOW()', req.body.summary, req.body.body, req.body.recommend, req.body.reviewer_name, req.body.reviewer_email]
+
+    db.query(reviewsPostQuery, postParams)
+      .then(result => {
+        res.send();
+      })
+      .catch(err => {
+        console.log(err)
       });
   }
 };
--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const db = require('./db');
 const port = 3000;
 const app = express();
 
-const {getReviews, getMetaData} = require('./queries');
+const {getReviews, getMetaData, updateHelpful, updateReport, createReview} = require('./queries');
 
 app.use(express.json());
 
 app.get('/reviews', getReviews);
 
 app.get('/reviews/meta', getMetaData);
+
+app.put('/reviews/:review_id/helpful', updateHelpful);
+
+app.put('/reviews/:review_id/report', updateReport);
+
+app.post('/reviews', createReview);
 
 app.listen(port, function() {
   console.log('Listening on port 3000');
