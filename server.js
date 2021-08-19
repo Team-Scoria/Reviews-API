99856e44197d8dfcfe7d87da6c15ee885375a6cb
shannamurry <shanna.murry84@gmail.com>
2021-08-19 16:28:48 -0400

    -

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const db = require('./db');
-const loader = require('./loader_config.txt')
 const port = 3000;
 const app = express();
 
-const {getReviews, getMetaData, updateHelpful, updateReport, createReview} = require('./queries');
+const { getReviews, getMetaData, updateHelpful, updateReport, createReview } = require('./queries');
 
 app.use(express.json());
 
@@ -19,9 +18,9 @@ app.put('/reviews/:review_id/report', updateReport);
 app.post('/reviews', createReview);
 
 app.get('/loaderio-7298e7b27c163fb6fe7bdddb47850c60.txt', (req, res) => {
-  res.send(loader);
+  res.send('loaderio-7298e7b27c163fb6fe7bdddb47850c60');
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Listening on port 3000');
 });
